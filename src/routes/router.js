815d221c296af1b484c33393c80dfd7ec2b5277e
Karim Hassou <karim.hassou@grenoble-inp.org>
2022-04-18 19:23:05 +0200

    progress

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 router.use(require('./user'))
 router.use(require('./product'))
+//frontend rendering
 router.get('/', function (req, res){
-    res.sendFile(__dirname+'/../frontend/index.html')
+    res.sendFile(__dirname+'/../frontend/projet.html')
 })
 module.exports = router
