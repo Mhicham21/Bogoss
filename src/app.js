25f9126dcdd44cfa9a1a3891a97d4cd8d5daaeb2
Karim Hassou <karim.hassou@grenoble-inp.org>
2022-04-18 18:58:27 +0200

    progress

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const app = express()
 // Configure Express App Instance
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+app.use(express.static('./frontend'))
 
 // Configure custom logger middleware
 app.use(logger.dev, logger.combined)
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
 router.use(require('./user'))
 router.use(require('./product'))
+router.get('/', function (req, res){
+    res.sendFile('index.html')
+})
 module.exports = router
